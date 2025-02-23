25cf507c35d77be0227253eba4d8856ed594d3b6
Kelvinvida99 <kelvinvida99@gmail.com>
2025-02-22 22:57:35 -0400

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
-import 'package:flutter/material.dart';
-import 'package:hello_world_app/presentation/screens/counter_fuctions_screen.dart';
-//import 'package:hello_world_app/presentation/screens/counter_screen.dart';
+import 'package:flutter/material.dart'; // Importa el paquete de Flutter para usar widgets de Material Design.
+import 'package:hello_world_app/presentation/screens/counter_fuctions_screen.dart'; // Importa la pantalla de funciones del contador.
+//import 'package:hello_world_app/presentation/screens/counter_screen.dart'; // Importa la pantalla del contador (comentada).
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); // La función principal que ejecuta la aplicación MyApp.
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key}); // Constructor de la clase MyApp.
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
-      home: CounterFuntionsScreen(),
+      debugShowCheckedModeBanner: false, // Oculta la etiqueta de modo debug.
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue), // Configura el tema de la aplicación.
+      home: CounterFuntionsScreen(), // Establece la pantalla principal de la aplicación.
     );
   }
 }
